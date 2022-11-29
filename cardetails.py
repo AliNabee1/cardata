e49f367b021b9a51fc9955c5f0b73da96d404716
AliNabee1 <92624045+AliNabee1@users.noreply.github.com>
2022-11-29 16:16:59 +0500

    Update cardetails.py

--- cardetails.py
+++ cardetails.py
@@ -32,15 +32,15 @@ def human_typer(driver_arg, xpath, text: str):
 chrome_options= webdriver.ChromeOptions()  
 chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 driver= webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
-driver.get('https://www.google.com')
-print(driver.page_source)    
+# driver.get('https://www.google.com')
+# print(driver.page_source)    
 # options = Options()
 # options.headless = True
 # driver = webdriver.Chrome(options=options)
-# driver.get('https://www.rapidcarcheck.co.uk/')
+driver.get('https://www.rapidcarcheck.co.uk/')
 driver.maximize_window()
 def carDetails():
     import pygsheets
